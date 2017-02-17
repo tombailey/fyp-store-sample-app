1572ca2270a441ed8006a56399cacc443cbc8266
Tom Bailey <tombailey1@hotmail.co.uk>
2017-02-17 23:26:40 +0000

    Initial commit

---
+++ app/src/main/java/me/tombailey/store/sampleapp/MainActivity.java
@@ -0,0 +1,18 @@
+package me.tombailey.store.sampleapp;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by tomba on 17/02/2017.
+ */
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_main);
+    }
+}
